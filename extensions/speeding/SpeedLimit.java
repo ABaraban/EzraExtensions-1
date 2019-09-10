7dc79794198cec4d1f4444b1814d8587bd854120
asherbaraban <asher.baraban@wustl.edu>
2019-09-10 12:12:24 -0500

    Finished Speed Limit!!!!!

--- extensions/speeding/SpeedLimit.java
+++ extensions/speeding/SpeedLimit.java
@@ -10,7 +10,7 @@ public class SpeedLimit {
         int actualSpeed = ap.nextInt("How fast were you going?");
         int speedDifferential = actualSpeed - speedLimit;
         int addedFine = speedDifferential - 10;
-        int fine = (speedDifferential < 0) ? 0 : (speedDifferential < 10) ? 50 : 50 + (addedFine * 10);
+        int fine = (speedDifferential <= 0) ? 0 : (speedDifferential < 10) ? 50 : 50 + (addedFine * 10);
         System.out.println("You reported a speed of " + actualSpeed + " MPH for a speed limit of " + speedLimit + "");
         System.out.println("You went " + speedDifferential + " over the speed limit.");
         System.out.println("Your fine is " + fine + "$.");
