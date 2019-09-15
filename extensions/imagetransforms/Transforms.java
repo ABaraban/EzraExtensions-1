d33656e0f34a1c16bbfd4c225a24fbd16f290396
asherbaraban <asher.baraban@wustl.edu>
2019-09-15 13:51:54 -0500

    Speed Limit (graded), Filters completed 1.2 and 1.3 2.1 almost:, RPS completed

--- extensions/imagetransforms/Transforms.java
+++ extensions/imagetransforms/Transforms.java
@@ -33,22 +33,70 @@ public class Transforms {
 	}
 	
 	public static void flipVert(Picture source, Picture target) {
+		for(int x = 0; x<source.width(); x++){
+			for(int y=0;y<source.height();y++){
+				int otherY = source.height() -1 -y;
+				Color d = source.get(x,otherY);
+				target.set(x,y,d);
+			}
+		}
 		// FIXME
 	}
 	
 	
 	public static void flipHorizLeftHalf(Picture source, Picture target) {
+		for(int x=0; x<source.width(); x++) {
+			for (int y= 0; y<source.height(); y++){
+				if (x>(source.width()/2)) {
+					int leftHalf = source.width() - 1 - x;
+					Color e = source.get(leftHalf, y);
+					target.set(x, y, e);
+				}
+				else {
+					Color c = source.get(x,y);
+					target.set(x,y,c);
+				}
+			}
+		}
 		// FIXME
 	}
 	
 	public static void flipVertBotHalf(Picture source, Picture target) {
+		for(int x=0; x<source.width(); x++) {
+			for (int y= 0; y<source.height(); y++){
+				if (y>(source.height()/2)) {
+					int flip = source.height() - 1 - y;
+					Color e = source.get(x, flip);
+					target.set(x, y, e);
+				}
+				else {
+					Color c = source.get(x,y);
+					target.set(x,y,c);
+				}
+			}
+		}
+
 		// FIXME
 	}
 	
 	public static void gradient(Picture target) {
+		System.out.println(target.height() + "height");
+		System.out.println(target.width() + "width");
+		for(int x=0; x<target.width(); x++) {
+			int amountRed = target.width()*x;
+			for (int y= 0; y<target.height(); y++){
+				int amountGreen = target.height()*y;
+				Color c = new Color(amountRed, amountGreen, 128);
+				target.set(x, y, c);
+				}
+
+			}
+
+		}
+
+
 		// FIXME
-				
-	}
+
 	
 	public static void edgeDetect(Picture source, Picture target) {
 		// FIXME
