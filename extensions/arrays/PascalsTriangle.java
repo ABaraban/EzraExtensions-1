c1d3a774adddcc2e69572e8083bf3aca2e5ffff7
asherbaraban <asher.baraban@wustl.edu>
2019-09-22 14:43:41 -0500

    Pascal's complete

--- extensions/arrays/PascalsTriangle.java
+++ extensions/arrays/PascalsTriangle.java
@@ -5,38 +5,74 @@ public class PascalsTriangle {
 
 	public static void main(String[] args) {
 		ArgsProcessor ap = new ArgsProcessor(args);
+		int entry = 0;
 		int n = ap.nextInt("Number of rows?");
 		int[][] array = new int[n][n];
-		System.out.println("      Column");
-		System.out.print("    ");
+		System.out.println("          Column");
+		System.out.print("       ");
 		for (int a = 0; a < n; ++a) {
-			System.out.print(a + "  ");
+			System.out.print(a + "    ");
 		}
 		System.out.println();
+		System.out.println("--------------------------------------------------------");
 		for (int r = 0; r < n; ++r) {
-			System.out.print(r + "  |");
+			if (r<10) {
+				System.out.print(r + "   |");
+			}
+			else {
+				System.out.print(r + "  |");
+			}
 			for (int c = 0; c < n; ++c) {
 				if (c == 0) {
 					array[r][c] = 1;
-					System.out.print(array[r][c] + "  ");
+					entry = array[r][c];
+					if (entry < 10) {
+						System.out.print("  " + array[r][c] + "  ");
+					}
 				}
-				if (r == c) {
-					array[r][c] = 1;
-					System.out.print(array[r][c] + "  ");
-				}
+//				if (r == c) {
+//					array[r][c] = 1;
+//					entry = array[r][c];
+//					if (entry < 10 && entry !=0) {
+//						System.out.print("  " + array[r][c] + "  ");
+//					}
+			//}
 				if (r < 0 || c < 0 || c > r) {
 					array[r][c] = 0;
-					if (array[c][r] == 0) {
-						System.out.print("   ");
-					}
+					//entry = array[r][c];
+						//if (array[r][c] == 0)
+							System.out.print("     ");
+
+
 				}
 				if (r != 0 && c != 0) {
 					array[r][c] = array[r - 1][c] + array[r - 1][c - 1];
-					if (array[c][r] == 0) {
-						System.out.print("   ");
+					entry = array[r][c];
+					if (entry < 10) {
+						if (array[r][c] == 0) {
+							System.out.print("     ");
+						}
+						else {
+							System.out.print("  " + array[r][c] + "  ");
+						}
+					}
+					if (entry <100 && entry >=10) {
+						if (array[r][c] == 0) {
+							//System.out.println("r,c"+array[r][c]);
+							System.out.print("     ");
+						}
+						else {
+							System.out.print(" " + array[r][c] + "  ");
+						}
 					}
 					else {
-						System.out.print(array[r][c] + "  ");
+						if (entry > 100) {
+							if (array[r][c] == 0 || array[r][c] == 1) {
+								//System.out.println("r,c"+array[r][c]);
+								System.out.print("     ");
+							} else
+								System.out.print(array[r][c] + "  ");
+						}
 					}
 				}
 
